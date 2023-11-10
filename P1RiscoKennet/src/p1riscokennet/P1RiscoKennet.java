b9e69cc2d21d52fe0fe5e6398ce5cb2ab8497752
KennyG04 <150395760+KennyG04@users.noreply.github.com>
2023-11-10 11:17:05 -0500

    Add files via upload

---
+++ P1RiscoKennet/src/p1riscokennet/P1RiscoKennet.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package p1riscokennet;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ESPE
+ */
+public class P1RiscoKennet {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner scanner = new Scanner(system.in);
+        
+        System.out.print( "Ingrese el primer numero");
+        double numero1= scanner.nextDouble();
+        
+        System.out.print("Ingrese el segundo numero");
+        double numero2= scanner.nextDouble();
+        
+        double suma = numero1+numero2;
+        
+        system.out.println("La suma de " + numero 1 "y "+ numero2+ "es: " + suma);
+    }
+    
+}
